d7f8c08c0c0bebf85e3f368c4cf3dab28567dc96
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-05-17 23:48:07 +0530

    Array assignment

--- array_main.go
+++ array_main.go
@@ -3,13 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	a := [5]int{1, 2, 3, 4, 5}
-	fmt.Println(a)
-	b := a[1:4] // slice
-	fmt.Println(b)
+	hobbies := [3]string{"skating", "painting", "studying"}
+	fmt.Println(hobbies)
+	fmt.Println(hobbies[0])
+	new_hobbies := hobbies[1:]
+	fmt.Println(new_hobbies)
 
-	// dynamic arrays -> to add element (append), to delete any element, use slice
-	c := []int{}
-	c = append(c, 20, 34)
-	fmt.Println(c)
+	// adding content of a list to another
+	b := []int{1,3,4,5}
+	c := []int{2,4,6,1}
+	b = append(b, c...)
+	fmt.Println(b, c)
+
 }
+
+// Time to practice what you learned!
+
+// 1) Create a new array (!) that contains three hobbies you have
+// 		Output (print) that array in the command line.
+// 2) Also output more data about that array:
+//		- The first element (standalone)
+//		- The second and third element combined as a new list
+// 3) Create a slice based on the first element that contains
+//		the first and second elements.
+//		Create that slice in two different ways (i.e. create two slices in the end)
+// 4) Re-slice the slice from (3) and change it to contain the second
+//		and last element of the original array.
+// 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
+// 6) Set the second goal to a different one AND then add a third goal to that existing dynamic array
+// 7) Bonus: Create a "Product" struct with title, id, price and create a
+//		dynamic list of products (at least 2 products).
+//		Then add a third product to the existing list of products.
