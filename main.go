ab3e88062690d528d3cbb632f99e844309d714c0
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-05-19 12:26:59 +0530

    Single Channel and Closing

--- main.go
+++ main.go
@@ -14,24 +14,31 @@ func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan) //helps go to identify when to close the channel so that for loop in main
+	// can stop waiting for new messages from the channel.
+	// But for this approach, we must know the longest time consuming operation to close the channel
 }
 
 func main() {
-	// doneChan := make(chan bool)
-	doneChans := make([]chan bool, 4)
+	doneChan := make(chan bool)
+	// doneChans := make([]chan bool, 4)
 
-	doneChans[0] = make(chan bool)
-	go greet("Nice to meet you!", doneChans[0])
-	doneChans[1] = make(chan bool)
-	go greet("How are you?", doneChans[1])
-	doneChans[2] = make(chan bool)
-	go slowGreet("How ... are ... you ...?", doneChans[2])
-	doneChans[3] = make(chan bool)
-	go greet("I hope you're liking the course!", doneChans[3])
+	// doneChans[0] = make(chan bool)
+	go greet("Nice to meet you!", doneChan)
+	// doneChans[1] = make(chan bool)
+	go greet("How are you?", doneChan)
+	// doneChans[2] = make(chan bool)
+	go slowGreet("How ... are ... you ...?", doneChan)
+	// doneChans[3] = make(chan bool)
+	go greet("I hope you're liking the course!", doneChan)
 
-	for _, doneChan := range doneChans {
-		<- doneChan
+	for doneChannel := range doneChan {
+		fmt.Println(doneChannel)
 	}
+
+	// for _, doneChan := range doneChans {
+	// 	<- doneChan
+	// }
 }
 
 // channel can be used to wait for the completion of a goroutine
